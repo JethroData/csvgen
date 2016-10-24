22ad9bfd88087642dfa2a2a185b6b7f712be5bca
shkediy <yuval.shkedi@gmail.com>
2016-10-23 20:07:16 -0400

    Added missing ']'

--- generateCsvgenIn.py
+++ generateCsvgenIn.py
@@ -148,11 +148,11 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"o:d:n:")
     except getopt.GetoptError:
-        sys.stderr.write('generateCsvgenIn.py [-o <output directory>] [-d <delimiter character>] [-n <null string> <table name> <statistics file>\n')
+        sys.stderr.write('generateCsvgenIn.py [-o <output directory>] [-d <delimiter character>] [-n <null string>] <table name> <statistics file>\n')
         sys.exit(2)  
     
     if len(args) < 2:
-        sys.stderr.write('generateCsvgenIn.py [-o <output directory>] [-d <delimiter character>] [-n <null string> <table name> <statistics file>\n')
+        sys.stderr.write('generateCsvgenIn.py [-o <output directory>] [-d <delimiter character>] [-n <null string>] <table name> <statistics file>\n')
         sys.exit(2)
         
     outdir = "."
